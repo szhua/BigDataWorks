5aeac4f1289555d2e692b3e8065496886a0c2619
szhua <389894467@qq.com>
2021-03-04 11:22:44 +0800

    test ssh

---
+++ src/main/java/com/szhua/mapreducetasks/muloutfile/OutPutFilesTask.java
@@ -0,0 +1,33 @@
+package com.szhua.mapreducetasks.muloutfile;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapreduce.RecordWriter;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.ReflectionUtils;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * @Author SZhua
+ * @Date 2021/3/4
+ * @Description: say st
+ */
+
+public class OutPutFilesTask  {
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
